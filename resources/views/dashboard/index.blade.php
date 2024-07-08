f3c2b0ed76eadcdb641994cc8662cadf70bcb811
kikiisa <kikiisa89@gmail.com>
2024-07-08 16:30:29 +0800

    feat:typo

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -43,23 +43,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                    <div class="stats-icon blue">
-                                        <i class="iconly-boldBookmark"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold mt-3">Jumlah Buku Di Pinjam</h6>
-                                    <h6 class="font-extrabold mb-0">40</h6>
-                                </div>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
+                
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
                         <div class="card-body px-4 py-4-5">
@@ -70,7 +54,7 @@
                                     </div>
                                 </div>
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold mt-3">Jumlah Buku</h6>
+                                    <h6 class="text-muted font-semibold mt-3">Jumlah Skripsi</h6>
                                     <h6 class="font-extrabold mb-0">{{$total_buku}}</h6>
                                 </div>
                             </div> 
